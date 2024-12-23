9616aa58b05699466fda821eb58854bc82b79119
Rob Hand <146272+sinon@users.noreply.github.com>
2024-12-23 22:14:34 +0000

    Scanning: String literals

---
+++ src/lib.rs
@@ -0,0 +1,216 @@
+use std::fmt;
+
+use miette::{miette, Error, LabeledSpan, Result};
+pub struct Token<'de> {
+    token_type: TokenType,
+    origin: &'de str,
+}
+
+#[derive(PartialEq)]
+pub enum TokenType {
+    // single-character
+    RightParen,
+    LeftParen,
+    RightBrace,
+    LeftBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+    // one or two character tokens
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    // literals
+    String,
+    Identifier,
+    Number(f64),
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    // Misc
+    Eof,
+}
+
+impl<'de> fmt::Display for Token<'de> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let i = self.origin;
+        match self.token_type {
+            TokenType::RightParen => write!(f, "RIGHT_PAREN {i} null"),
+            TokenType::LeftParen => write!(f, "LEFT_PAREN {i} null"),
+            TokenType::Eof => write!(f, "EOF  null"),
+            TokenType::RightBrace => write!(f, "RIGHT_BRACE {i} null"),
+            TokenType::LeftBrace => write!(f, "LEFT_BRACE {i} null"),
+            TokenType::Comma => write!(f, "COMMA {i} null"),
+            TokenType::Dot => write!(f, "DOT {i} null"),
+            TokenType::Minus => write!(f, "MINUS {i} null"),
+            TokenType::Plus => write!(f, "PLUS {i} null"),
+            TokenType::Star => write!(f, "STAR {i} null"),
+            TokenType::Semicolon => write!(f, "SEMICOLON {i} null"),
+            TokenType::Equal => write!(f, "EQUAL {i} null"),
+            TokenType::EqualEqual => write!(f, "EQUAL_EQUAL {i} null"),
+            TokenType::Bang => write!(f, "BANG {i} null"),
+            TokenType::BangEqual => write!(f, "BANG_EQUAL {i} null"),
+            TokenType::Less => write!(f, "LESS {i} null"),
+            TokenType::LessEqual => write!(f, "LESS_EQUAL {i} null"),
+            TokenType::Greater => write!(f, "GREATER {i} null"),
+            TokenType::GreaterEqual => write!(f, "GREATER_EQUAL {i} null"),
+            TokenType::Slash => write!(f, "SLASH {i} null"),
+            TokenType::String => write!(f, "STRING \"{i}\" {i}"),
+            TokenType::Identifier => todo!(),
+            TokenType::Number(_) => todo!(),
+            TokenType::And => todo!(),
+            TokenType::Class => todo!(),
+            TokenType::Else => todo!(),
+            TokenType::False => todo!(),
+            TokenType::Fun => todo!(),
+            TokenType::For => todo!(),
+            TokenType::If => todo!(),
+            TokenType::Nil => todo!(),
+            TokenType::Or => todo!(),
+            TokenType::Print => todo!(),
+            TokenType::Return => todo!(),
+            TokenType::Super => todo!(),
+            TokenType::This => todo!(),
+            TokenType::True => todo!(),
+            TokenType::Var => todo!(),
+            TokenType::While => todo!(),
+        }
+    }
+}
+
+pub struct Lexer<'de> {
+    whole: &'de str,
+    rest: &'de str,
+    byte: usize,
+}
+
+impl<'de> Lexer<'de> {
+    pub fn new(input: &'de str) -> Self {
+        Self {
+            rest: input,
+            whole: input,
+            byte: 0,
+        }
+    }
+}
+
+impl<'de> Iterator for Lexer<'de> {
+    type Item = Result<Token<'de>, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let mut chars = self.rest.char_indices();
+            let (at, c) = chars.next()?;
+            let c_str = &self.rest[at..at + c.len_utf8()];
+            let c_onwards = self.rest;
+            self.rest = chars.as_str();
+            self.byte += c.len_utf8();
+
+            enum Started {
+                String,
+                Number,
+                Identifier,
+                IfNextEqual(TokenType, TokenType),
+            }
+
+            let res = move |t_type: TokenType| {
+                Some(Ok(Token {
+                    token_type: t_type,
+                    origin: c_str,
+                }))
+            };
+
+            let started = match c {
+            '(' => return res(TokenType::LeftParen),
+            ')' => return res(TokenType::RightParen),
+            '{' => return res(TokenType::LeftBrace),
+            '}' => return res(TokenType::RightBrace),
+            ',' => return res(TokenType::Comma),
+            '.' => return res(TokenType::Dot),
+            '-' => return res(TokenType::Minus),
+            '+' => return res(TokenType::Plus),
+            '*' => return res(TokenType::Star),
+            ';' => return res(TokenType::Semicolon),
+            '"' => Started::String,
+            '0'..='9' => Started::Number,
+            'a'..='z' => Started::Identifier,
+            '<' => Started::IfNextEqual(TokenType::LessEqual, TokenType::Less),
+            '>' => Started::IfNextEqual(TokenType::GreaterEqual, TokenType::Greater),
+            '!' => Started::IfNextEqual(TokenType::BangEqual, TokenType::Bang),
+            '=' => Started::IfNextEqual(TokenType::EqualEqual, TokenType::Equal),
+            c if c.is_whitespace() => continue,
+            _ => {
+                    return Some(Err(
+                        miette! {labels = vec![LabeledSpan::at(self.byte-c.len_utf8()..self.byte, "this character")], 
+                        "Unexpected token '{c}' in input"}
+                        .with_source_code(self.whole.to_string()),
+                    ))
+                }
+            };
+
+            match started {
+                Started::String => {
+                    if !self.rest.contains("\"") {
+                        return Some(Err(
+                        miette! {labels = vec![LabeledSpan::at(self.byte -c.len_utf8()..self.byte, "this unterminated string")],
+                        "Unterminated string in input"
+                    }.with_source_code(self.whole.to_string())
+                    ));
+                    } else {
+                        let (s, _) = self.rest.split_once("\"")?;
+                        let c_str = &c_onwards[1..s.len() + 1];
+                        self.byte += s.len() + 1;
+                        self.rest = &self.rest[s.len() + 1..];
+                        return Some(Ok(Token {
+                            token_type: TokenType::String,
+                            origin: c_str,
+                        }));
+                    }
+                }
+                Started::Number => todo!(),
+                Started::Identifier => todo!(),
+                Started::IfNextEqual(then, else_t) => {
+                    if self.rest.starts_with("=") {
+                        let c_str = &c_onwards[..2];
+                        self.byte += 1;
+                        self.rest = &self.rest[1..];
+                        let token = Token {
+                            token_type: then,
+                            origin: c_str,
+                        };
+                        return Some(Ok(token));
+                    } else {
+                        let token = Token {
+                            token_type: else_t,
+                            origin: c_str,
+                        };
+                        return Some(Ok(token));
+                    }
+                }
+            };
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,143 +1,40 @@
-use core::fmt;
-use std::env;
+use clap::Subcommand;
+use codecrafters_interpreter::Lexer;
+use miette::{IntoDiagnostic, Result, WrapErr};
 use std::fs;
-use std::io::{self, Write};
+use std::path::PathBuf;
 use std::process::ExitCode;
 
-#[derive(PartialEq, Eq)]
-enum Atom {
-    RightParen,
-    LeftParen,
-    RightBrace,
-    LeftBrace,
-    Comma,
-    Dot,
-    Minus,
-    Plus,
-    Star,
-    SemiColon,
-    Eof,
-    Equals,
-    EqualEquals,
-    Bang,
-    BangEquals,
-    Less,
-    LessEquals,
-    Greater,
-    GreaterEquals,
-    Slash,
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    commands: Commands,
+}
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Tokenize { filename: PathBuf },
 }
 
-impl fmt::Debug for Atom {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Atom::RightParen => write!(f, "RIGHT_PAREN ) null"),
-            Atom::LeftParen => write!(f, "LEFT_PAREN ( null"),
-            Atom::Eof => write!(f, "EOF  null"),
-            Atom::RightBrace => write!(f, "RIGHT_BRACE }} null"),
-            Atom::LeftBrace => write!(f, "LEFT_BRACE {{ null"),
-            Atom::Comma => write!(f, "COMMA , null"),
-            Atom::Dot => write!(f, "DOT . null"),
-            Atom::Minus => write!(f, "MINUS - null"),
-            Atom::Plus => write!(f, "PLUS + null"),
-            Atom::Star => write!(f, "STAR * null"),
-            Atom::SemiColon => write!(f, "SEMICOLON ; null"),
-            Atom::Equals => write!(f, "EQUAL = null"),
-            Atom::EqualEquals => write!(f, "EQUAL_EQUAL == null"),
-            Atom::Bang => write!(f, "BANG ! null"),
-            Atom::BangEquals => write!(f, "BANG_EQUAL != null"),
-            Atom::Less => write!(f, "LESS < null"),
-            Atom::LessEquals => write!(f, "LESS_EQUAL <= null"),
-            Atom::Greater => write!(f, "GREATER > null"),
-            Atom::GreaterEquals => write!(f, "GREATER_EQUAL >= null"),
-            Atom::Slash => write!(f, "SLASH / null"),
-        }
-    }
-}
+fn main() -> Result<ExitCode> {
+    let args = Args::parse();
 
-/*
-LEFT_PAREN ( null
-LEFT_PAREN ( null
-RIGHT_PAREN ) null
-EOF  null
- */
-
-fn main() -> ExitCode {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return ExitCode::FAILURE;
-    }
-
-    let command = &args[1];
-    let filename = &args[2];
-
-    match command.as_str() {
-        "tokenize" => {
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
-                String::new()
-            });
-            let mut return_code = 0;
-            if !file_contents.is_empty() {
-                for (ln_num, ln) in file_contents.lines().enumerate() {
-                    let mut char_iter = ln.chars().peekable();
-                    while let Some(c) = char_iter.next() {
-                        match c {
-                            ' ' | '\t' => {
-                                continue;
-                            }
-                            '(' => println!("{:?}", Atom::LeftParen),
-                            ')' => println!("{:?}", Atom::RightParen),
-                            '{' => println!("{:?}", Atom::LeftBrace),
-                            '}' => println!("{:?}", Atom::RightBrace),
-                            ',' => println!("{:?}", Atom::Comma),
-                            '.' => println!("{:?}", Atom::Dot),
-                            '-' => println!("{:?}", Atom::Minus),
-                            '+' => println!("{:?}", Atom::Plus),
-                            '*' => println!("{:?}", Atom::Star),
-                            ';' => println!("{:?}", Atom::SemiColon),
-                            '=' | '!' | '>' | '<' | '/' => {
-                                let (current_atom, peek_check_ahead, two_char_atom) = match c {
-                                    '=' => (Atom::Equals, '=', Atom::EqualEquals),
-                                    '!' => (Atom::Bang, '=', Atom::BangEquals),
-                                    '>' => (Atom::Greater, '=', Atom::GreaterEquals),
-                                    '<' => (Atom::Less, '=', Atom::LessEquals),
-                                    '/' => (Atom::Slash, '/', Atom::Slash),
-                                    _ => panic!(),
-                                };
-                                if let Some(n) = char_iter.peek() {
-                                    if *n == peek_check_ahead && two_char_atom != Atom::Slash {
-                                        println!("{:?}", two_char_atom);
-                                        char_iter.next();
-                                    } else if *n == peek_check_ahead && two_char_atom == Atom::Slash
-                                    {
-                                        break;
-                                    } else {
-                                        println!("{:?}", current_atom)
-                                    }
-                                } else {
-                                    println!("{:?}", current_atom)
-                                }
-                            }
-                            _ => {
-                                eprintln!(
-                                    "[line {}] Error: Unexpected character: {}",
-                                    ln_num + 1,
-                                    c
-                                );
-                                return_code = 65;
-                            }
-                        }
-                    }
+    match args.commands {
+        Commands::Tokenize { filename } => {
+            let input = fs::read_to_string(filename)
+                .into_diagnostic()
+                .wrap_err_with(|| format!("reading file"))?;
+            if !input.is_empty() {
+                let lexer = Lexer::new(&input);
+                for token in lexer {
+                    let token = token?;
+                    println!("{token}");
                 }
             }
-            println!("{:?}", Atom::Eof);
-            return ExitCode::from(return_code);
-        }
-        _ => {
-            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return ExitCode::FAILURE;
+            println!("EOF  null");
+            return Ok(ExitCode::from(0));
         }
     }
 }
